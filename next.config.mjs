2d0a4a48c146613ae7471e34df0bb16a01664394
psv90 <74047356+psv90@users.noreply.github.com>
2025-08-11 15:41:14 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
+import path from 'node:path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      '@components': path.resolve(process.cwd(), 'components'),
+      '@lib': path.resolve(process.cwd(), 'lib'),
+      '@store': path.resolve(process.cwd(), 'store'),
+      '@types': path.resolve(process.cwd(), 'types'),
+      '@utils': path.resolve(process.cwd(), 'utils'),
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
